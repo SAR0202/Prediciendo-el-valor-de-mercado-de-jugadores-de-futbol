6525cd4c9b991b3e94ff1ce3b142995e15a585d3
SAR0202 <sebastianar1978@gmail.com>
2025-12-08 17:03:56 -0300

    division_y_preproceso.py

--- division_y_preproceso.py
+++ division_y_preproceso.py
@@ -7,21 +7,22 @@ from sklearn.impute import SimpleImputer
 
 def division_y_preproceso(direccion_df_final):
     df_final = pd.read_csv(direccion_df_final)
-
+    #variables a USAR
     X = df_final.drop("valor_real", axis=1)
     y = df_final["valor_real"]
 
     categoricas = X.select_dtypes(include=["object"]).columns.to_list()
     numericas = X.select_dtypes(include=["int64", "float64"]).columns.tolist()
 
-    # division del dataset
+    # DIVISION del dataset
     X_train, X_test, y_train, y_test = train_test_split(
         X, y,
         test_size=0.2,
         random_state=123)
-
+    
     preproceso = ColumnTransformer(transformers=[
         ("cat", OneHotEncoder(drop="first", handle_unknown="ignore", sparse_output=False), categoricas),
         ("num", SimpleImputer(strategy="constant", fill_value=0), numericas)  #
     ])
+
     return X_train, X_test, y_train, y_test, preproceso
